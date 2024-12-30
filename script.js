f09190da4d7833a93f09e1726dbdc7e094f63730
Katharine Green <186521265+kbetchelle@users.noreply.github.com>
2024-12-30 09:55:01 -0800

    createGrid working, formatting for littleDiv req

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
 const bigDiv = document.querySelector("#gridContainer");
 const gridSizePrompt = document.querySelector("#sizeInput");
 
-gridSizePrompt.addEventListener('click', () => {
-    let sizeSelected = prompt("Enter a number between 1-100: ")
-    if (sizeSelected > 100 || sizeSelected <1) {
-        alert("ERROR.Please enter a number between 1 and 100")
-    console.log(sizeSelected)
-    }
-});
-
-
+//CREATING GRID 
 function makeGrid (sizeSelected) {
     if (bigDiv.hasChildNodes()) {
         bigDiv.removeChild(bigDiv.childNodes[0]);
     }
-    for (i=0; i < sizeSelected; i ++) {
-        const divRow = document.createElement("div");
+    for (let i = 0; i < sizeSelected; i ++) {
+        divRow = document.createElement("div");
         divRow.classList.add("divRow");
         bigDiv.appendChild(divRow);
 
-        for (i = 0; i < sizeSelected; i++){
-            let littleDivWidth = 550/parseInt(sizeSelected)
+        for (let i = 0; i < sizeSelected; i++){
+            let containerSize = bigDiv.height;
+            let littleDivWidth = containerSize/parseInt(sizeSelected);
             littleDiv = document.createElement("div");
+            littleDiv.classList.add('littleDiv')
             littleDiv.style.width = littleDivWidth;
             littleDiv.style.height = littleDivWidth;
             divRow.appendChild(littleDiv);
         }
     }
-
-    //uses sizeSelected to create divs
-    //has a mouseenters event listener to change the colors of the divs
 }
 
-makeGrid(4);
+function getGridSize () {
+    let sizeSelected = prompt("Enter a number between 1-100: ");
+    if (sizeSelected > 100 || sizeSelected <1) {
+        alert("ERROR.Please enter a number between 1 and 100");
+    } else {
+        if (bigDiv.hasChildNodes()){
+            while (bigDiv.firstChild) {
+                bigDiv.removeChild(bigDiv.firstChild)};
+            makeGrid(sizeSelected); 
+        } else {
+            makeGrid(sizeSelected);
+        }
+    }
+};
 
+gridSizePrompt.addEventListener('click', getGridSize);
+
+
+//ADDING HOVER ELEMENTS 
 const randColor = () => {
     return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
 }
